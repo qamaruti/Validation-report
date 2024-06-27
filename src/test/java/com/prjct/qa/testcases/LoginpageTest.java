cbc20a81352a362904131bdd39fcd72c449ef2d6
Marutik <mkokane@bastiansolutions.com>
2024-06-27 11:19:43 +0530

    petvalu

---
+++ src/test/java/com/prjct/qa/testcases/LoginpageTest.java
@@ -0,0 +1,48 @@
+package com.prjct.qa.testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.prjct.qa.base.TestBase;
+import com.prjct.qa.pages.HomePage;
+import com.prjct.qa.pages.LoginPage;
+
+public class LoginpageTest extends TestBase{
+	LoginPage loginpage;
+	HomePage homepage;
+	public LoginpageTest() {
+		super();
+	}
+	
+	@BeforeMethod
+	public void setup() {
+		
+		initilization();
+		 loginpage=new LoginPage();
+	}
+	
+	@Test
+	public void loginpagetitletest() {
+		String title=loginpage.validateloginpageTitle();
+		Assert.assertEquals(title, "Login - Exacta Portal");
+	}
+	@Test
+	public void crmlogotest() {
+		boolean flag=loginpage.validatelogo();
+		Assert.assertTrue(flag);
+	}
+	@Test
+	public void loginTest() {
+		homepage = loginpage.login(prop.getProperty("username"), prop.getProperty("password"));
+	}
+	
+	@AfterMethod
+	public void teardown() {
+		driver.close();
+	}
+	
+}
